9ce88149bace09de5ab7ff246086b728d0642342
ChengEx <frf2isgood@gmail.com>
2022-04-30 10:30:43 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
+</script>
+
+<template>
+  <Navbar/>
+  <RouterView />
+</template>
+
+<style>
+#app {
+  font-family : Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing : antialiased;
+  -moz-osx-font-smoothing : grayscale;
+  text-align : center;
+  color : #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/views/ProductView.vue
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+        <h1>Hi this is product</h1>
+    </div>
+</template>
